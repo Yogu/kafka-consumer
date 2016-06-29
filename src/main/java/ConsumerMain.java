ebabf132c8f2efa06595adb70c03e5977273b8a9
Jan Melcher <mail@jan-melcher.de>
2016-06-29 22:29:12 +0200

    Add logging

--- src/main/java/ConsumerMain.java
+++ src/main/java/ConsumerMain.java
@@ -4,22 +4,30 @@ import com.sun.net.httpserver.HttpServer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public class ConsumerMain {
 	private static String lastLog = "";
 
 	public static void main(String[] args) throws IOException {
+		System.out.println("Starting http server...");
 		HttpServer server = HttpServer.create(new InetSocketAddress(9000), 0);
 		server.createContext("/", new MyHandler());
 		server.setExecutor(null); // creates a default executor
 		server.start();
+		System.out.println("Listening on port 9000");
 
+		if (System.getenv("INPUT_input") == null) {
+			throw new RuntimeException("Missing environment variable INPUT_input");
+		}
 
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "kafka:9092");
@@ -29,14 +37,20 @@ public class ConsumerMain {
 		props.put("session.timeout.ms", "30000");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		System.out.println("Creating consumer...");
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-
-		if (System.getenv("INPUT_input") == null) {
-			throw new RuntimeException("Missing environment variable INPUT_input");
+		System.out.println("Created consumer");
+		for (Map.Entry<String, List<PartitionInfo>> topic : consumer.listTopics().entrySet()) {
+			System.out.println("  topic " + topic.getKey() + " with " + topic.getValue().size() + " partitions: ");
+			for (PartitionInfo partition : topic.getValue()) {
+				System.out.println("    partition " + partition.partition() + ", leader: " + partition.leader().host());
+			}
 		}
 
 		String[] topicNames = System.getenv("INPUT_input").split(",");
+		System.out.println("Subscribing to " + System.getenv("INPUT_input"));
 		consumer.subscribe(Arrays.asList(topicNames));
+		System.out.println("Waiting for messages...");
 
 		while (true) {
 			ConsumerRecords<String, String> records = consumer.poll(100);
