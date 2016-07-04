ebcddfabc6ebdf52b9534752d2836a6967f5980a
Martin Root <mail@martin-root.de>
2016-07-04 17:25:57 +0200

    Update consumer main class

--- src/main/java/ConsumerMain.java
+++ src/main/java/ConsumerMain.java
@@ -1,49 +1,71 @@
+import com.siemens.ct.exi.exceptions.EXIException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
 import de.unistuttgart.isw.serviceorchestration.api.MessageBus;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.PartitionInfo;
+import de.unistuttgart.isw.serviceorchestration.api.MessageSender;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.Date;
 
 public class ConsumerMain {
-	private static String lastLog = "";
+    private static String lastXml = "";
+    private static String lastValue = "";
+    private static String lastDate = "";
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("Starting http server...");
-		HttpServer server = HttpServer.create(new InetSocketAddress(9000), 0);
-		server.createContext("/", new MyHandler());
-		server.setExecutor(null); // creates a default executor
-		server.start();
-		System.out.println("Listening on port 9000");
+    public static void main(String[] args) throws IOException, EXIException, SAXException, InterruptedException {
+        // opcfoundation does not like Java default user agent
+        System.setProperty("http.agent", "Mozilla/5.0");
+        HttpServer server = HttpServer.create(new InetSocketAddress(9000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
 
-		MessageBus bus = new MessageBus();
-		bus.createReceiver("input", "https://opcfoundation.org/UA/2008/02/Types.xsd",
-				xml -> {
-					lastLog = xml;
-					System.out.println(xml);
-				});
+        MessageBus bus = new MessageBus();
+        bus.createReceiver("input", "https://opcfoundation.org/UA/2008/02/Types.xsd",
+                xml -> {
+                    lastXml = xml;
+                    XStream xstream = new XStream(new StaxDriver());
+                    xstream.alias("Float", Float.class);
 
-		bus.runListener();
-	}
+                    Float value = (Float) xstream.fromXML(lastXml);
 
-	static class MyHandler implements HttpHandler {
-		@Override
-		public void handle(HttpExchange t) throws IOException {
-			String response = lastLog;
-			t.sendResponseHeaders(200, response.length());
-			OutputStream os = t.getResponseBody();
-			os.write(response.getBytes());
-			os.close();
-		}
-	}
+                    lastValue = value.toString();
+
+                    lastDate= new Date().toString();
+                });
+
+        bus.runListener();
+
+
+
+
+
+
+//        MessageSender sender = bus.createSender("output", "https://opcfoundation.org/UA/2008/02/Types.xsd");
+//
+//        while (true) {
+//            sender.send(xmlFile);
+//            Thread.sleep(1000);
+//        }
+
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "xml: " + lastXml + "\n" + "value: " + lastValue + "\n" + "lastUpdate: " + lastDate;
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+
 }
